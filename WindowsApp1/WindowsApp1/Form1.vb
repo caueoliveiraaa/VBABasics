c94f8e9fcae69c8cc2a26f56ffead8dcb4aae1db
Cauê Oliveira <77469726+caueoliveiraaa@users.noreply.github.com>
2024-08-10 18:30:15 -0300

    Feat: Name of app and comments

--- WindowsApp1/WindowsApp1/Form1.vb
+++ WindowsApp1/WindowsApp1/Form1.vb
@@ -2,6 +2,7 @@
 
 
     Private Sub BtnStart_Click(sender As Object, e As EventArgs) Handles BtnStart.Click
+        ' The code here runs when the BtnStart gets clicked
 
         MsgBox("Hello World")
         MsgBox("How are you?")
