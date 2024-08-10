2137fa8ce88ba5ebfc129bbffd7fd641254ba7af
Cauê Oliveira <77469726+caueoliveiraaa@users.noreply.github.com>
2024-08-10 20:20:16 -0300

    feat: Data Types
    This commit implements data types in a button to demonstrate the most basic VBA data types.

--- WindowsApp1/02OutputAndVariables/Form1.Designer.vb
+++ WindowsApp1/02OutputAndVariables/Form1.Designer.vb
@@ -24,12 +24,13 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.btnGo = New System.Windows.Forms.Button()
         Me.BntVariables = New System.Windows.Forms.Button()
+        Me.btnDataTypes = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'btnGo
         '
         Me.btnGo.BackColor = System.Drawing.SystemColors.ActiveCaption
-        Me.btnGo.Location = New System.Drawing.Point(259, 62)
+        Me.btnGo.Location = New System.Drawing.Point(259, 26)
         Me.btnGo.Name = "btnGo"
         Me.btnGo.Size = New System.Drawing.Size(248, 105)
         Me.btnGo.TabIndex = 0
@@ -39,18 +40,29 @@ Partial Class Form1
         'BntVariables
         '
         Me.BntVariables.BackColor = System.Drawing.SystemColors.MenuHighlight
-        Me.BntVariables.Location = New System.Drawing.Point(259, 207)
+        Me.BntVariables.Location = New System.Drawing.Point(259, 151)
         Me.BntVariables.Name = "BntVariables"
-        Me.BntVariables.Size = New System.Drawing.Size(248, 91)
+        Me.BntVariables.Size = New System.Drawing.Size(248, 104)
         Me.BntVariables.TabIndex = 1
         Me.BntVariables.Text = "Variables Demonstration"
         Me.BntVariables.UseVisualStyleBackColor = False
+        '
+        'btnDataTypes
+        '
+        Me.btnDataTypes.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.btnDataTypes.Location = New System.Drawing.Point(259, 277)
+        Me.btnDataTypes.Name = "btnDataTypes"
+        Me.btnDataTypes.Size = New System.Drawing.Size(248, 104)
+        Me.btnDataTypes.TabIndex = 2
+        Me.btnDataTypes.Text = "Data Types"
+        Me.btnDataTypes.UseVisualStyleBackColor = False
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnDataTypes)
         Me.Controls.Add(Me.BntVariables)
         Me.Controls.Add(Me.btnGo)
         Me.Name = "Form1"
@@ -61,4 +73,5 @@ Partial Class Form1
 
     Friend WithEvents btnGo As Button
     Friend WithEvents BntVariables As Button
+    Friend WithEvents btnDataTypes As Button
 End Class
--- WindowsApp1/02OutputAndVariables/Form1.vb
+++ WindowsApp1/02OutputAndVariables/Form1.vb
@@ -9,6 +9,7 @@
 
     End Sub
 
+
     Private Sub BntVariables_Click(sender As Object, e As EventArgs) Handles BntVariables.Click
 
 
@@ -30,6 +31,48 @@
         stSecondName = "Smith"
 
         MsgBox("Hello, " & stFirstName & " " & stSecondName & " welcome.")
+
+
+    End Sub
+
+
+    Private Sub btnDataTypes_Click(sender As Object, e As EventArgs) Handles btnDataTypes.Click
+
+
+        ' "i" is used for integer variables
+        ' "b" is used for boolean variables
+        ' "dec" is used for decimal variables
+        ' "dt" is used for date variables
+        Dim stMake As String
+        Dim stModel As String
+        Dim stColor As String
+        Dim iDoors As Integer
+        Dim iEngineSize As Integer
+        Dim decPrice As Decimal
+        Dim bTaxed As Boolean
+        Dim dtDateRegistered As Date
+
+        ' Use # to assign a date value to a date variable
+        stMake = "Ford"
+        stModel = "Escort"
+        stColor = "Red"
+        iDoors = 5
+        bTaxed = True
+        iEngineSize = 1200
+        decPrice = 999.99
+        dtDateRegistered = #08/10/2024# ' American format
+
+        ' use vbNewLine constant to break lines
+        MsgBox(
+            "Make: " & stMake & vbNewLine &
+            "Model: " & stModel & vbNewLine &
+            "Doors: " & iDoors & vbNewLine &
+            "Engine Size:" & iEngineSize & vbNewLine &
+            "Color: " & stColor & vbNewLine &
+            "Taxed: " & bTaxed & vbNewLine &
+            "Price: " & decPrice & vbNewLine &
+            "Date Registration: " & dtDateRegistered
+        )
 
 
     End Sub
