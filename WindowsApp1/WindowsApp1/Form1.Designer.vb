7cb51bbd16cad5ace3f318c691d96610455b6c7e
Cauê Oliveira <77469726+caueoliveiraaa@users.noreply.github.com>
2024-08-10 17:37:57 -0300

    feat: Hello world
    Hello world message boxes

---
+++ WindowsApp1/WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,51 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BtnStart = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'BtnStart
+        '
+        Me.BtnStart.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.BtnStart.Location = New System.Drawing.Point(260, 163)
+        Me.BtnStart.Name = "BtnStart"
+        Me.BtnStart.Size = New System.Drawing.Size(262, 123)
+        Me.BtnStart.TabIndex = 0
+        Me.BtnStart.Text = "Press Here"
+        Me.BtnStart.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.BtnStart)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents BtnStart As Button
+End Class
---
+++ WindowsApp1/WindowsApp1/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+
+
+    Private Sub BtnStart_Click(sender As Object, e As EventArgs) Handles BtnStart.Click
+
+        MsgBox("Hello World")
+        MsgBox("How are you?")
+        MsgBox("I hope you are well")
+
+    End Sub
+
+
+End Class
